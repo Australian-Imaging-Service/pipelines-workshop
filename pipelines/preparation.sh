ba51365119d1b0a894893de26892cceae7a3f1d6
Thomas G. Close <tom.g.close@gmail.com>
2025-08-06 13:33:00 +0930

    updated workshop tutorial

--- pipelines/preparation.sh
+++ pipelines/preparation.sh
@@ -4,25 +4,23 @@
 #echo 'export PATH=$HOME/.local/bin:$PATH' >> ~/.bashrc
 
 # Clone the pipelines-community repository
-mkdir -p ~/git
+
 if [ ! -d ~/git/pipelines-community ]; then
-    git clone https://github.com/Australian-Imaging-Service/pipelines-community.git ~/git/pipelines-community
+    git clone https://github.com/Australian-Imaging-Service/pipelines-community.git ~/pipelines-community
 fi
-pushd ~/git/pipelines-community
+pushd ~/pipelines-community
 
 # Update the pipelines-community repository
 git pull
 
 # Install the pipelines-community repository
-pip install -r ./tutorial/requirements.txt
+pip install -r ./requirements.txt
 python -m bash_kernel.install
 
 
 # Pre-build/pull the required XNAT docker images to save time
-xnat4tests -c ./tutorial/xnat4tests-config.yaml start --with-data openneuro-t1w
-xnat4tests -c ./tutorial/xnat4tests-config.yaml stop
+xnat4tests start --with-data openneuro-t1w
+xnat4tests stop
 pydra2app make xnat ./specs/australian-imaging-service-community/examples/zip.yaml --spec-root ./specs  --for-localhost
 pydra2app make xnat ./specs/australian-imaging-service-community/examples/bet.yaml --spec-root ./specs  --for-localhost
-docker pull vnmd/freesurfer_7.1.1
-docker pull freesurfer/synthstrip:1.6
 popd
